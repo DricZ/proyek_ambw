5d80072136cccc7dc72842bb95553e2f54228066
Kwonsangelic <124767182+Kwonsangelic@users.noreply.github.com>
2023-06-22 22:01:35 +0700

    format, manifest, sw

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('app-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.php',
+          '/assets/css/styles.css',
+          '/js/main.js',
+          '/assets/icon.jpg'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
